70d5aac4f95d1f98760df33146ed8891e8dd436c
Manisekharan Chellamuthu <manisekhar2000@yahoo.com>
2020-04-21 21:30:23 -0400

    Update BubbleSort

--- src/main/java/algorithms/sort/BubbleSort.java
+++ src/main/java/algorithms/sort/BubbleSort.java
@@ -1,59 +1,87 @@
-package coding.algorithms.sort;
+package algorithms.sort;
+
+import java.util.*;
 
 public class BubbleSort {
-    public static void main(String args[]){
-        BubbleSort bubbleSort = new BubbleSort();
-        int unsorted[] = {64, 34, 25, 1};
-        int sorted[] = bubbleSort.sort(unsorted);
-        System.out.println("Sorted array");
-        bubbleSort.printArray(sorted);
+    public static void main(String[] args) {
+        int[] array = {5, 1, 4,0,2,-1};
+        System.out.println("Giving unsorted Array : "+ Arrays.toString(array));
+        //bubbleSort_for(array);
+        bubbleSort_while(array);
+        //bubbleSort_rec(array, array.length);
+    }
 
-        recursiveSort(unsorted, unsorted.length);
-        System.out.println("Sorted array (Using recursive)");
-        bubbleSort.printArray(unsorted);
+    private static void bubbleSort_for(int[] array) {
+        int temp = 0;
+        int length = array.length;
+        boolean swapped;
+        System.out.println("Length : "+length);
+
+        for(int i=0; i < length - 1; i++){
+            swapped = false;
+            System.out.println(i + " = "+Arrays.toString(array));
+            for(int j=0; j < length-i-1; j++){
+                System.out.println("Comparing "+ array[j]+ " and " + array[j+1]);
+                if(array[j] > array[j+1]) {
+                    temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                    swapped = true;
+                    System.out.println("Swapping Elements: New Array After Swap");
+                    System.out.println("      "+j + " = "+Arrays.toString(array));
+                }
+
+            }
+            if(swapped == false){
+                break;
+            }
         }
 
-    private int[] sort(int[] arr) {
-        int length = arr.length;
-        int innerLoop = 0;
-        int outerLoop = 0;
-        for(int i=0; i < length-1; i++){
-            outerLoop++;
-            for(int j=0; j < length-i-1; j++){
-                innerLoop++;
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr [j+1];
-                    arr[j+1] = temp;
+        System.out.println("Bubble sort : "+Arrays.toString(array));
+        //Arrays.stream(array).forEach(System.out::println);
+    }
+
+    private static void bubbleSort_while(int[] array) {
+        int temp = 0;
+        int length = array.length;
+        boolean swapped = true;
+        System.out.println("Length : "+length);
+
+        while(swapped){
+            swapped = false;
+            for(int j=0; j < length-1; j++){
+                System.out.println("Comparing "+ array[j]+ " and " + array[j+1]);
+                if(array[j] > array[j+1]) {
+                    temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                    swapped = true;
+                    System.out.println("Swapping Elements: New Array After Swap");
+                    System.out.println("      "+j + " = "+Arrays.toString(array));
                 }
             }
-
         }
-        System.out.println("Outer loop count : "+outerLoop);
-        System.out.println("Inner loop count : "+innerLoop);
-        return arr;
+
+        System.out.println("Bubble sort : "+Arrays.toString(array));
+        //Arrays.stream(array).forEach(System.out::println);
     }
 
-    private static void recursiveSort(int[] unsorted, int length) {
-        if(length == 1){
+    private static void bubbleSort_rec(int array[], int n)
+    {
+        // Base case
+        if (n == 1){
+            System.out.println("Sorted Array : "+Arrays.toString(array));
             return;
         }
-        for(int i=0; i<length-1; i++){
-            if(unsorted[i] > unsorted[i+1]){
-                int temp = unsorted[i];
-                unsorted[i] = unsorted[i+1];
-                unsorted[i+1] = temp;
+
+        for (int i=0; i<n-1; i++)
+            if (array[i] > array[i+1])
+            {
+                int temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
             }
-        }
-        recursiveSort(unsorted, length-1);
 
-    }
-
-    private void printArray(int[] arr) {
-        int n = arr.length;
-        for (int i=0; i<n; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        bubbleSort_rec(array, n-1);
     }
 }
